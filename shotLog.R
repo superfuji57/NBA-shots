3e78d649b648f304a0d44529cf18753bd98ba520
superfuji57 <andy.won@gmail.com>
2015-03-04 14:51:38 -0500

    minor updates

--- shotLog.R
+++ shotLog.R
@@ -1,15 +1,18 @@
 library(httr)
 library(rvest)
+library(jsonlite)
 
 load("./data/player.table.Rda")
 
 playerSearch <- function(player_name = ""){
-        id <- player.table[player.table$display_first_last==player_name, 1]
+        id <- player.table[player.table$display_first_last==player_name, person_id]
+        if (length(id) == 0) return("No player found")
         return(id)
 }
 
+# data goes back to 2013-14 season
 shotStatsURL <- function(playerID=951, from="", to="", gameSegment="", nGames=0, location="",month=0, outcome="",
-                         period=0, season="", seasonSegment="", seasonType="Regular+Season",
+                         period=0, season="2014-15", seasonSegment="", seasonType="Regular+Season",
                          teamID=0, vsConference="", vsDivision="") {
         shots.url <- paste0("http://stats.nba.com/stats/playerdashptshotlog?DateFrom=", from,
                             "&DateTo=", to, 
@@ -31,15 +34,15 @@ shotStatsURL <- function(playerID=951, from="", to="", gameSegment="", nGames=0,
         return(shots.url)
 }
 
+test <- shotStatsURL(playerSearch("Kevin Garnett"), season="2012-13")
 # request the URL and parse the JSON
-request <- GET(shots.url)
+request <- GET(test)
 
 status_code(request)
 headers(request)
 str(content(request))
 head(content(request))
 
-library(jsonlite)
 content <- content(request, "text")
 john.wall <- fromJSON(content)
 lapply(john.wall, dim)
